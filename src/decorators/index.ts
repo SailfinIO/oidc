41a8e73761a1715b3adfdfebc52195eaefd99ff5
Michael Curtis <mpbcurtis@gmail.com>
2025-01-15 17:35:52 -0400

    fix: decorator exports

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,10 +1,7 @@
 // src/decorators/index.ts
 
-export * from './automaticRefresh';
-export * from './requiresAuthentication';
-export * from './requiresClaim';
-export * from './requiresScopes';
 export * from './MetadataManager';
 export * from './KeyFactory';
 export * from './oidcLogin';
 export * from './oidcCallback';
+export * from './protected';
